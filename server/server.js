34f88103b6c1a8d26d0b7efb736b7139875dc4ce
debubz <bao.es.hoang2@gmail.com>
2021-04-22 03:09:46 +1000

    installing some backend dependencies

--- server/server.js
+++ server/server.js
@@ -4,44 +4,43 @@ const logging = require("./config/logging");
 const path = require("path");
 const config = require("./config/config");
 
-/* 
-    Server stuff
-*/
 require("dotenv").config();
 
-// config
+/**
+ * TODO:
+ * - set up cors to secure access
+ */
+
+// server config ------------------------------------------------------------------------------------------
 const NAMESPACE = "SERVER";
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// database connection ------------------------------------------------------------------------------------------
+n;
 
 // logging each request
 app.use((req, res, next) => {
     logging.info(NAMESPACE, `Method - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]`);
-
     res.on("finish", () => {
         logging.info(
             NAMESPACE,
             `Method - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}], STATUS - [${req.statusCode}]`
         );
     });
-
     next();
 });
 
-// middleware
-app.use(express.json());
-
-// serving build folder
+// serving static file ------------------------------------------------------------------------------------------
 app.use(express.static(path.resolve(__dirname, "../web", "build")));
-
 app.use(express.static("public"));
 
-// ================================================================================
-// api imports
+// api routing ------------------------------------------------------------------------------------------
 const sampleRoute = require("./routes/sample");
-const { copyFile } = require("fs");
 
 app.use("/api/sample", sampleRoute);
-// ================================================================================
+// api routing end ------------------------------------------------------------------------------------------
 
 // error handling
 app.use((req, res) => {
@@ -54,14 +53,8 @@ app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../web", "build", "index.html"));
 });
 
-// mongodb connection
-
-// creating server
+// listening to server ------------------------------------------------------------------------------------------
 const httpServer = http.createServer(app);
 httpServer.listen(config.server.port, () => {
     logging.info(NAMESPACE, `Server running on ${config.server.hostname}:${config.server.port}`);
 });
-
-// app.listen(config.server.port, () => {
-//     logging.info(NAMESPACE, `Server running on ${config.server.hostname}:${config.server.port}`);
-// });
