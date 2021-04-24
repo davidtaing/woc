ed21cd39033d4a38460ae4c22d0b3c82f2712fa3
deBubz <bao.es.hoang2@gmail.com>
2021-04-24 16:57:31 +1000

    setting up jwt and passport strategies - need to test

---
+++ server/config/passport.js
@@ -0,0 +1,41 @@
+const passport = require("passport")
+const localStrategy = require('passport-local').Strategy
+
+const logging = require('./logging')
+const UserModel = require('../models/user.model')
+
+
+/* 
+    setting up passport middleware
+*/
+const NAMESPACE = "PASSPORT"
+
+passport.use(
+    "/login",
+    new localStrategy(
+        {
+            usernameField: 'email',
+            passwordField: 'password',
+        },
+        async (email, password, done) => {
+            try {
+
+            const user = await UserModel.findOne({ email });
+            if(!user) {
+                return done(null, false, { msg: 'user not found '});
+            }
+
+            const validate = await user.isValidPassword(password);
+            if(!validate) {
+                return done(null, false, { msg: 'wrong password '});
+            } 
+
+            // login OK
+            return done(null, user, { msg: 'logged in successfully '})
+            } catch(error) {
+                logging.error(NAMESPACE, `error logging in ${error.message}`, error);
+                return done(error);
+            }
+        }
+    )
+)
