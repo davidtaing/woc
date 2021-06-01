981a732353d2468b4277d67416174c2e24611027
deBubz <bao.es.hoang2@gmail.com>
2021-06-01 11:47:58 +1000

    added base middleware for verification

---
+++ server/validator/runValidator.js
@@ -0,0 +1,21 @@
+const { validationResult } = require('express-validator');
+/* 
+    Validation middleware
+*/
+
+/**
+ *  middleware validating the body content
+ */
+module.exports.runValidation = (req, res, next) => {
+    const err = validationResult(req);
+
+    // failed validation
+    if (!err.isEmpty())
+        return res.status(400).json({ error: mapError(err.errors) });
+
+    next();
+};
+
+// mapping list of failed validations
+const mapError = (errors) =>
+    errors.map((e) => ({ param: e.param, msg: e.msg }));
