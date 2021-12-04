915e554ea95ff493b56295e734250c6d3be3dfdf
debubz <bao.es.hoang2@gmail.com>
2021-12-04 14:22:56 +1100

    api doc notes

--- server/env.js
+++ server/env.js
@@ -6,3 +6,13 @@
 */
 
 process.env.NODE_ENV = 'dev';
+
+/*
+
+So generally the arguements are I could either attach swagger UI or de couple it from the project 
+There are pros and cons with either methodology [more here](https://stackoverflow.com/questions/31300756/can-swagger-autogenerate-its-yaml-based-on-existing-express-routes)
+
+another guide on [coupling swagger IO with express to generate swagger io](https://stackoverflow.com/questions/31300756/can-swagger-autogenerate-its-yaml-based-on-existing-express-routes)
+
+
+*/
