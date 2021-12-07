86053135e9451dc44aaa24e591f90822967a1e0b
debubz <bao.es.hoang2@gmail.com>
2021-12-07 18:55:06 +1100

    dev - fix events redering problem

--- server/controller/events.controller.js
+++ server/controller/events.controller.js
@@ -1,40 +1,33 @@
-const https = require('https')
+const https = require('https');
 
 let currentDate = new Date();
 currentDate.setMonth(currentDate.getMonth() - 2);
 
 console.log(currentDate);
 
-const token = process.env.API_KEY; 
+const token = process.env.API_KEY;
 const options = {
     hostname: 'api.humanitix.com',
     path: '/v1/events?page=1&pageSize=100&since=' + currentDate.toISOString(),
     method: 'GET',
-        headers: {
-            'X-Api-Key': token
-        }
-}
-console.log(options.path);
-const getEvents = (req,res) => {
+    headers: {
+        'X-Api-Key': token,
+    },
+};
+
+const getEvents = (req, res) => {
     https.get(options, (response) => {
-        
         var result = '';
-        response.on('data', function (chunk) {
+        response.on('data', (chunk) => {
             result += chunk;
         });
 
-        response.on('end', function () {    
-            var finalResponse=JSON.parse(result);
-            
-        res.status(200).json(result);
+        response.on('end', () => {
+            res.status(200).json(result);
         });
-
     });
-
 };
 
 module.exports = {
     getEvents,
 };
-
-
