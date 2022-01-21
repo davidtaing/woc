e2c206cdb6572578f4ca0de259e825a7142da98a
Ksenija Lunic <dasicksenija@gmail.com>
2022-01-21 19:02:11 +1100

    add random list of mentors

---
+++ web/src/pages/userDashboard/mentors/mentors.jsx
@@ -0,0 +1,13 @@
+const mentors = [
+    { name: "Alexandra Robinson", photo: "" },
+    { name: "Roberta Lee", photo: "" },
+    { name: "Clara Boyd", photo: "" },
+    { name: "Christina Martin", photo: "" },
+    { name: "Sofia Ronald", photo: "" },
+    { name: "Anna Frank", photo: "" },
+    { name: "Melissa Loyd", photo: "" },
+    { name: "Layla N. Martinovic", photo: "" },
+    { name: "Ruth Nolan", photo: "" },
+];
+
+export default mentors;
